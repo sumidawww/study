ef0010c58cf830e7df2b4288c396c19bae21062c
隅田 浩司 <sumidahiroshi@sumida-no-MacBook-Air.local>
2013-01-12 14:37:27 +0900

    Initial Commit

---
+++ study20120112/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  study20120112
+//
+//  Created by 隅田 浩司 on 2013/01/12.
+//  Copyright (c) 2013年 hiroshi sumida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ study20120112/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  study20120112
+//
+//  Created by 隅田 浩司 on 2013/01/12.
+//  Copyright (c) 2013年 hiroshi sumida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
